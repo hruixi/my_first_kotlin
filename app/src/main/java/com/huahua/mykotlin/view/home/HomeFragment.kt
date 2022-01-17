aa8d5b29f78f2434895e72b30fa9940ea298dbc2
ruixiangHe <hrx5350@closeli.cn>
2022-01-17 19:54:09 +0800

    学习 https://mp.weixin.qq.com/s/QXzWQZknJlGto9w9rs9nqA

--- app/src/main/java/com/huahua/mykotlin/view/home/HomeFragment.kt
+++ app/src/main/java/com/huahua/mykotlin/view/home/HomeFragment.kt
@@ -4,37 +4,56 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Adapter
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager.widget.PagerAdapter
+import androidx.viewpager2.widget.ViewPager2
 import com.huahua.mykotlin.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
-
     private lateinit var homeViewModel: HomeViewModel
     private var _binding: FragmentHomeBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         homeViewModel =
             ViewModelProvider(this).get(HomeViewModel::class.java)
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
-
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+        initView()
         return root
+    }
+
+    private fun initView() {
+        val pagerAdapter = object : RecyclerView.Adapter<HomeViewHolder>() {
+            override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HomeViewHolder {
+                TODO("Not yet implemented")
+            }
+
+            override fun onBindViewHolder(holder: HomeViewHolder, position: Int) {
+                TODO("Not yet implemented")
+            }
+
+            override fun getItemCount(): Int {
+                TODO("Not yet implemented")
+            }
+        }
+
+        val pageTransformer = ViewPager2.PageTransformer { page, position ->
+            
+        }
+
+        binding.bliVp.adapter = pagerAdapter
     }
 
     override fun onDestroyView() {
